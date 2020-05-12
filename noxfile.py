ae98e44f344511e8ddd21de7218bfbc7b2018fb6
staticdev <thiagocavila@gmail.com>
2020-05-12 15:59:03 -0300

    new import checks

--- noxfile.py
+++ noxfile.py
@@ -53,7 +53,6 @@ def lint(session: Session) -> None:
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
-        "flake8-import-order",
     )
     session.run("flake8", *args)
 
