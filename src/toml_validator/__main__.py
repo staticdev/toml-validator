666914a685a5f439b103aaf2658d14718a59dba8
staticdev <thiagocavila@gmail.com>
2021-01-18 21:02:04 +0100

    Add directory and multifiles support

--- src/toml_validator/__main__.py
+++ src/toml_validator/__main__.py
@@ -1,19 +1,51 @@
 """Command-line interface."""
+import pathlib
 import sys
+from typing import Iterable
+from typing import Iterator
+from typing import Set
+from typing import Tuple
 
 import click
 
-from . import validation
+import toml_validator.use_cases.toml as toml
+
+
+def _get_recursive_supported_files(
+    paths: Iterable[pathlib.Path],
+) -> Iterator[pathlib.Path]:
+    """Recursively return the path to supported files."""
+    for path in paths:
+        if path.is_dir():
+            yield from _get_recursive_supported_files(path.iterdir())
+        elif path.is_file() and toml.validate_extension(str(path)):
+            yield path
+
+
+def get_files(src: Tuple[str, ...]) -> Set[pathlib.Path]:
+    """Return all paths from list of arguments."""
+    files = set()
+    for i in src:
+        p = pathlib.Path(i)
+        if p.is_file():
+            files.add(p)
+        else:
+            files.update(_get_recursive_supported_files(p.iterdir()))
+    return files
 
 
 @click.command()
-@click.argument("filename", type=click.Path(exists=True))
+@click.argument(
+    "src",
+    nargs=-1,
+    type=click.Path(exists=True, file_okay=True, dir_okay=True, readable=True),
+)
 @click.version_option()
-def main(filename: str) -> None:
+def main(src: Tuple[str, ...]) -> None:
     """Makes validations and echos errors if found.
 
     Args:
-        filename: name of validated file.
+        src: name of validated file.
 
     Return status:
     * 0: no errors found
@@ -21,16 +53,25 @@ def main(filename: str) -> None:
     * 2: invalid path
     * 3: errors found
     """
-    validation.validate_extension(filename)
+    files = get_files(src)
 
-    click.secho("Reading file {}.".format(filename), fg="blue")
+    if not files:
+        click.secho("No file to check.")
+        sys.exit(0)
 
-    errors = validation.validate_toml(filename)
-    if errors:
-        click.secho("Error(s) found: {}.".format(errors), fg="red")
+    errors_found = False
+    for file in files:
+        click.secho(f"Reading file {file}.", fg="blue")
+
+        errors = toml.execute(str(file))
+        if errors:
+            click.secho(f"Error(s) found on file {file}: {errors}.", fg="red")
+            errors_found = True
+        else:
+            click.secho(f"No problems found parsing file {file}!", fg="green")
+
+    if errors_found:
         sys.exit(3)
-    else:
-        click.secho("No problems found parsing file {}!".format(filename), fg="green")
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 """Test cases for the __main__ module."""
+import os
+import pathlib
 from typing import Any
 from unittest.mock import Mock
 
@@ -16,37 +18,79 @@ def runner() -> click.testing.CliRunner:
 
 
 @pytest.fixture
-def mock_validation_validate_extension(mocker: MockFixture) -> Any:
-    """Fixture for mocking validation.validate_extension."""
-    return mocker.patch("toml_validator.validation.validate_extension")
+def mock_use_case_toml_validate_extension(mocker: MockFixture) -> Any:
+    """Fixture for mocking toml.validate_extension."""
+    return mocker.patch("toml_validator.use_cases.toml.validate_extension")
 
 
 @pytest.fixture
-def mock_validation_validate_toml_no_error(mocker: MockFixture) -> Any:
-    """Fixture for mocking validation.validate_toml with no errors."""
-    mock = mocker.patch("toml_validator.validation.validate_toml")
+def mock_use_case_toml_no_error(mocker: MockFixture) -> Any:
+    """Fixture for mocking toml.execute with no errors."""
+    mock = mocker.patch("toml_validator.use_cases.toml.execute")
     mock.return_value = ""
     return mock
 
 
 @pytest.fixture
-def mock_validation_validate_toml_with_error(mocker: MockFixture) -> Any:
-    """Fixture for mocking validation.validate_toml with error."""
-    mock = mocker.patch("toml_validator.validation.validate_toml")
+def mock_use_case_toml_with_error(mocker: MockFixture) -> Any:
+    """Fixture for mocking toml.execute with error."""
+    mock = mocker.patch("toml_validator.use_cases.toml.execute")
     mock.return_value = "|some error description|"
     return mock
 
 
+def test_get_files_file(runner: click.testing.CliRunner) -> None:
+    """It returns same file."""
+    with runner.isolated_filesystem():
+        with open("file.toml", "w") as f:
+            f.write("content doesnt matter")
+
+        src = "file.toml"
+        expected = [pathlib.Path("file.toml")]
+        sources = list(__main__.get_files((src,)))
+        assert sorted(expected) == sorted(sources)
+
+
+def test_get_files_directory(runner: click.testing.CliRunner) -> None:
+    """It returns file in directory."""
+    with runner.isolated_filesystem():
+        os.mkdir("dir")
+        with open("dir/file.toml", "w") as f:
+            f.write("content doesnt matter")
+
+        src = "dir"
+        expected = [pathlib.Path("dir/file.toml")]
+        sources = list(__main__.get_files((src,)))
+        assert sorted(expected) == sorted(sources)
+
+
+def test_get_files_nested_directory(runner: click.testing.CliRunner) -> None:
+    """It returns file in subdirectory."""
+    with runner.isolated_filesystem():
+        os.mkdir("dir")
+        os.mkdir("dir/dir2")
+        with open("dir/file.notoml", "w") as f:
+            f.write("content doesnt matter")
+        with open("dir/dir2/file.toml", "w") as f:
+            f.write("content doesnt matter")
+
+        src = "dir"
+        expected = [pathlib.Path("dir/dir2/file.toml")]
+        sources = list(__main__.get_files((src,)))
+        assert sorted(expected) == sorted(sources)
+
+
 def test_main_without_argument(runner: click.testing.CliRunner) -> None:
-    """It exits with a status code of 2."""
+    """It exits with a status code of 0."""
     result = runner.invoke(__main__.main)
-    assert result.exit_code == 2
+    assert result.output == "No file to check.\n"
+    assert result.exit_code == 0
 
 
 def test_main_with_argument_success(
     runner: click.testing.CliRunner,
-    mock_validation_validate_extension: Mock,
-    mock_validation_validate_toml_no_error: Mock,
+    mock_use_case_toml_validate_extension: Mock,
+    mock_use_case_toml_no_error: Mock,
 ) -> None:
     """It exits with a status code of zero."""
     with runner.isolated_filesystem():
@@ -62,8 +106,8 @@ def test_main_with_argument_success(
 
 def test_main_with_invalid_path(
     runner: click.testing.CliRunner,
-    mock_validation_validate_extension: Mock,
-    mock_validation_validate_toml_with_error: Mock,
+    mock_use_case_toml_validate_extension: Mock,
+    mock_use_case_toml_with_error: Mock,
 ) -> None:
     """It outputs error."""
     with runner.isolated_filesystem():
@@ -77,8 +121,8 @@ def test_main_with_invalid_path(
 
 def test_main_with_errors(
     runner: click.testing.CliRunner,
-    mock_validation_validate_extension: Mock,
-    mock_validation_validate_toml_with_error: Mock,
+    mock_use_case_toml_validate_extension: Mock,
+    mock_use_case_toml_with_error: Mock,
 ) -> None:
     """It outputs error."""
     with runner.isolated_filesystem():
@@ -87,15 +131,7 @@ def test_main_with_errors(
 
         result = runner.invoke(__main__.main, ["file.toml"])
         assert result.output == (
-            "Reading file file.toml.\n" "Error(s) found: |some error description|.\n"
+            "Reading file file.toml.\n"
+            "Error(s) found on file file.toml: |some error description|.\n"
         )
         assert result.exit_code == 3
-
-
-@pytest.mark.e2e
-def test_main_without_arguments_in_production_env(
-    runner: click.testing.CliRunner,
-) -> None:
-    """It exits with a status code of 2 (e2e)."""
-    result = runner.invoke(__main__.main)
-    assert result.exit_code == 2
